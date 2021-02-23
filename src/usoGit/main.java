75b9e9802dd1c33c5603dd28598e3283e11b0199
kevin <kevinzamora239@gmail.com>
2021-02-23 18:07:48 -0300

    metodo hola

--- src/usoGit/main.java
+++ src/usoGit/main.java
@@ -1,7 +1,12 @@
 package usoGit;
+import usoGit.userShop;
 
 class main {
 	public static void main (String [] args) {
 		System.out.printf("nueva modificaicon");
-	}
+	
+    userShop nada;
+    nada = new userShop();
+    nada.hola();
+    }
 }
--- src/usoGit/userShop.java
+++ src/usoGit/userShop.java
@@ -3,7 +3,10 @@ package usoGit;
 public class userShop {
 	public userShop() {
 		
-		
 	}
 
+	public void hola() {
+		System.out.printf("hola de niue");
+		
+	}
 }
